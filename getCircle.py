8ecbf9b89458d1635bb77550f1839f8975288b06
Zhaoyang Hou <florianhou@hotmail.com>
2022-01-13 16:27:13 +0100

    前期处理

--- getCircle.py
+++ getCircle.py
@@ -1,13 +1,27 @@
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
 
-img_bgr = cv.imread("./pictures/25.bmp")
-print(np.shape(img_bgr))
+
+def showim(img):
+    cv.namedWindow("window", cv.WINDOW_NORMAL)
+    cv.imshow("window", img)
+    k = cv.waitKey(0)
+    if k == 'q':
+        cv.destroyAllWindows()
+    return 0
+
+
+img_raw = cv.imread("./pictures/25.bmp")
+img_bgr = img_raw.copy()
 img_lab = cv.cvtColor(img_bgr, cv.COLOR_BGR2LAB)
+img_rgb = cv.cvtColor(img_bgr, cv.COLOR_BGR2RGB)
+
+img = img_lab[:,:,1]
+# img = cv.equalizeHist(img)
+img = cv.GaussianBlur(img, (5,5),0)
+_, img = cv.threshold(img, 0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
 
 
-img = img_bgr
-cv.imshow("window", img)
-k = cv.waitKey()
-if k == 'q':
-    cv.destroyAllWindows()
+plt.imshow(img,cmap="gray")
+plt.show()
